ae6f3ced8ea7b18034d633dfd5bf18ffb99590bc
karlPet <karlpetter.svensson@hotmail.se>
2016-10-08 11:47:02 +0200

    database is automatically setup when scraper is used

--- root/scrape_single_website.js
+++ root/scrape_single_website.js
@@ -10,5 +10,29 @@ OpsFromMiddleWare.rawScrapeDataToDbOps(websiteName[0]).then(function(ops) {
   Database.batch(ops, function (err) {
       if (err) { return console.log('Ooops!', err); }
       console.log('All recipes succesfully stored!');
+
+      var siteList = [];
+      var amountOfScrapedRecipes = 0;
+
+      Database.createReadStream()
+          .on('data', function(data) {
+            if (data.key.includes('link')) {
+              amountOfScrapedRecipes += 1;
+            }
+            if (data.key.includes("recipeamt")) {
+              siteList.push(data.key.replace("recipeamt", ""));
+            }
+          })
+          .on('close', function() {
+            console.log("Process finished, found " + amountOfScrapedRecipes + " links.");
+            console.log("Found these scraped websites: " + siteList);
+            Database.put('numberofrecipes', amountOfScrapedRecipes, function(err) {
+              Database.put('sitelist', siteList, function(err) {
+                console.log("successfully stored amountOfScrapedRecipes and siteList!");
+                Database.close();
+              });
+            });
+          });
+
   });
 });
