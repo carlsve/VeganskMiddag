65c82865af968b263c151340be031bb5dd4bfa8e
karlpet <karlpetter.svensson@hotmail.se>
2016-12-12 22:41:28 +0100

    linking to mongolab cloud storage

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ var flash = require("connect-flash");
 var routes = require(__dirname + "/routes");
 
 var app = express();
-mongoose.connect("mongodb://localhost:27017/test");
+/*
+* Mongoose by default sets the auto_reconnect option to true.
+* We recommend setting socket options at both the server and replica set level.
+* We recommend a 30 second connection timeout because it allows for
+* plenty of time in most operating environments.
+*/
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+               replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
+var mongodbUri = 'mongodb://admin:admin@ds053186.mlab.com:53186/veganrecipes';
+
+mongoose.connect(mongodbUri, options);
 app.set("port", process.env.port || 3000);
 
 app.use(express.static(__dirname + '/public'));
--- libs/save-to-mongodb.js
+++ libs/save-to-mongodb.js
@@ -4,8 +4,18 @@ var mongoose = require("mongoose");
 var Recipe = require(__dirname + "/../model/recipe-model");
 var scraper = require(__dirname + "/sites/scraper");
 
-mongoose.connect("mongodb://localhost:27017/test");
+/*
+* Mongoose by default sets the auto_reconnect option to true.
+* We recommend setting socket options at both the server and replica set level.
+* We recommend a 30 second connection timeout because it allows for
+* plenty of time in most operating environments.
+*/
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+               replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
+var mongodbUri = 'mongodb://admin:admin@ds053186.mlab.com:53186/veganrecipes';
+
+mongoose.connect(mongodbUri, options);
 /*
 This method has problems. close with ctrl-z
 TODO: find a batch save package, because mongoose is bad.
